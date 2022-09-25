b9f7ae35d1761eae5068515909a329110bf3461b
Subho <contact.subhoghosh@gmail.com>
2022-09-25 12:53:30 +0530

    feat: basic re-resizable setup

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -1,35 +1,38 @@
 import { useCardStore } from "../store";
+import { Resizable } from "re-resizable";
 
 export default function TweetCard({ rootRef }: any) {
   const cardWidth = useCardStore((state: any) => state.width);
   const cardHeight = useCardStore((state: any) => state.width);
 
   return (
-    <div
-      ref={rootRef}
-      style={{ width: cardWidth }}
-      className="rounded-2xl bg-gradient-to-br from-yellow-300 to-red-500 py-16 px-20"
-    >
-      <div className="rounded-xl bg-white/50 px-8 py-7 backdrop-blur-md">
-        <div className="flex items-center gap-x-5">
-          <img
-            className="h-14 w-14 rounded-full"
-            src="https://pbs.twimg.com/profile_images/1510928172686225411/bTCh20YN_400x400.jpg"
-            alt="Andrej's pic"
-          />
-          <div>
-            <h2 className="text-zinc-900">Andrej</h2>
-            <p className="text-xs text-zinc-900">@reactive_dude</p>
+    <Resizable>
+      <div
+        ref={rootRef}
+        style={{ width: cardWidth }}
+        className="rounded-2xl bg-gradient-to-br from-yellow-300 to-red-500 py-16 px-20"
+      >
+        <div className="rounded-xl bg-white/50 px-8 py-7 backdrop-blur-md">
+          <div className="flex items-center gap-x-5">
+            <img
+              className="h-14 w-14 rounded-full"
+              src="https://pbs.twimg.com/profile_images/1510928172686225411/bTCh20YN_400x400.jpg"
+              alt="Andrej's pic"
+            />
+            <div>
+              <h2 className="text-zinc-900">Andrej</h2>
+              <p className="text-xs text-zinc-900">@reactive_dude</p>
+            </div>
+          </div>
+          <div className="mt-2 text-zinc-900">
+            <p>It&apos;s not possible to learn everything.</p>
+            <p className="mt-3">
+              Great software engineers are not those who know everything but
+              those who can solve problems and adapt.
+            </p>
           </div>
         </div>
-        <div className="mt-2 text-zinc-900">
-          <p>It&apos;s not possible to learn everything.</p>
-          <p className="mt-3">
-            Great software engineers are not those who know everything but those
-            who can solve problems and adapt.
-          </p>
-        </div>
       </div>
-    </div>
+    </Resizable>
   );
 }
