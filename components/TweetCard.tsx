c9eb65cc7be83745e3a8f7b440701f712758abff
Subho <contact.subhoghosh@gmail.com>
2022-09-27 16:50:50 +0530

    feat: ability to change tweet card bg from sidepanel gradients

--- components/TweetCard.tsx
+++ components/TweetCard.tsx
@@ -1,4 +1,4 @@
-import { useCardStore } from "../store";
+import { useCardStore, useGradientStore } from "../store";
 import { Resizable } from "re-resizable";
 
 export default function TweetCard({ rootRef }: any) {
@@ -7,6 +7,8 @@ export default function TweetCard({ rootRef }: any) {
 
   const changeWidth = useCardStore((state: any) => state.changeWidth);
   const changeHeight = useCardStore((state: any) => state.changeHeight);
+
+  const gradients = useGradientStore((state: any) => state.gradients);
 
   return (
     <Resizable
@@ -17,31 +19,38 @@ export default function TweetCard({ rootRef }: any) {
         changeHeight(cardHeight + d.height);
       }}
     >
-      <div
-        ref={rootRef}
-        className="flex h-full items-center justify-center overflow-hidden rounded-2xl bg-gradient-to-br from-yellow-300 to-red-500 py-16 px-20"
-      >
-        <div className="max-w-screen-sm rounded-xl bg-white/50 px-8 py-7 backdrop-blur-md">
-          <div className="flex items-center gap-x-5">
-            <img
-              className="h-14 w-14 rounded-full"
-              src="https://pbs.twimg.com/profile_images/1510928172686225411/bTCh20YN_400x400.jpg"
-              alt="Andrej's pic"
-            />
-            <div>
-              <h2 className="text-zinc-900">Andrej</h2>
-              <p className="text-xs text-zinc-900">@reactive_dude</p>
+      {gradients
+        .filter((gradient: any) => gradient.selected)
+        .map((gradient: any) => (
+          <div
+            ref={rootRef}
+            style={{
+              background: `linear-gradient(to bottom right, ${gradient.from}, ${gradient.to})`,
+            }}
+            className="flex h-full items-center justify-center overflow-hidden rounded-2xl py-16 px-20"
+          >
+            <div className="max-w-screen-sm rounded-xl bg-white/50 px-8 py-7 backdrop-blur-md">
+              <div className="flex items-center gap-x-5">
+                <img
+                  className="h-14 w-14 rounded-full"
+                  src="https://pbs.twimg.com/profile_images/1510928172686225411/bTCh20YN_400x400.jpg"
+                  alt="Andrej's pic"
+                />
+                <div>
+                  <h2 className="text-zinc-900">Andrej</h2>
+                  <p className="text-xs text-zinc-900">@reactive_dude</p>
+                </div>
+              </div>
+              <div className="mt-2 text-zinc-900">
+                <p>It&apos;s not possible to learn everything.</p>
+                <p className="mt-3">
+                  Great software engineers are not those who know everything but
+                  those who can solve problems and adapt.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="mt-2 text-zinc-900">
-            <p>It&apos;s not possible to learn everything.</p>
-            <p className="mt-3">
-              Great software engineers are not those who know everything but
-              those who can solve problems and adapt.
-            </p>
-          </div>
-        </div>
-      </div>
+        ))}
     </Resizable>
   );
 }
