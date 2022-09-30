0b3d63213ea7f4d0bcb6c831dc7b693c572fa563
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-09-30 22:24:07 +0530

    Added scalation of img when downloaded

--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -6,6 +6,8 @@ export default function ExportButton({ rootRef }: any) {
       const canvas = await html2canvas(rootRef.current, {
         allowTaint: true,
         useCORS: true,
+        scale: 3,
+        backgroundColor: null,
       });
       const img = canvas
         .toDataURL("image/png", 1.0)
