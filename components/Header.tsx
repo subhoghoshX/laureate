fde63b99f27047e713c4135aa267c1700855a957
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-10-01 14:45:10 +0530

    dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header({ setIsPanelOpen }: Props) {
 
   return (
     <header className="flex items-center justify-between gap-x-4 bg-white dark:bg-[#2c2c2c] px-4 py-2 shadow">
-      <img src={`/laureate-logo${theme=="dark"?"-darkmode":""}.svg`} alt="laureate logo" className="" />
+      <img src={`/laureate-logo.svg`} alt="laureate logo" className="" />
       <form className="w-1/2">
         <input
           className="w-full rounded-full bg-slate-200 dark:bg-[#3c3c3c] px-6 py-2.5 text-sm"
