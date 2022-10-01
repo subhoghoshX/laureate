274e484b6256853b5ef59684deb1dcde8f6cefcf
Subho <contact.subhoghosh@gmail.com>
2022-10-01 12:30:53 +0530

    feat: api front-end and cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,33 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 interface Props {
   setIsPanelOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function Header({ setIsPanelOpen }: Props) {
+  const [url, setUrl] = useState("");
+
+  async function handleSumbit(e: any) {
+    e.preventDefault();
+
+    const res = await fetch("/api/hello", {
+      method: "post",
+      body: JSON.stringify({ tweetUrl: url }),
+    });
+
+    const data = await res.json();
+  }
+
   return (
     <header className="flex items-center justify-between gap-x-4 bg-white px-4 py-2 shadow">
       <img src="/laureate-logo.svg" alt="laureate logo" className="" />
-      <form className="w-1/2">
+      <form className="w-1/2" onSubmit={handleSumbit}>
         <input
           className="w-full rounded-full bg-slate-200 px-6 py-2.5 text-sm"
           type="text"
           placeholder="Paste tweet link here"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
       </form>
       <div className="flex gap-x-3">
