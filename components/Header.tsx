17c0b77285e9e18c0b4a54a047244b74c59f879e
Mayank <mayank.ks.2002@gmail.com>
2022-10-04 16:37:54 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -99,8 +99,8 @@ export default function Header({ setIsPanelOpen }: Props) {
           </svg>
         </button>
         <aside
-          className={`h-90 shadow-[1px_1px_5px_rgba(0,0,0,0.25)] absolute w-60 translate-x-[calc(50%-20px)] right-0 top-14 rounded-md self-end overflow-hidden transition-transform duration-200 ease-out ${
-            isDemoOpen ? "translate-y-0" : "opacity-0 -translate-y-60 pointer-events-none"
+          className={`h-90 shadow-[1px_1px_5px_rgba(0,0,0,0.25)] absolute w-60 translate-x-[calc(50%-20px)] right-0 top-14 rounded-md self-end overflow-hidden transition-[transform,opacity] duration-150 ease-out ${
+            isDemoOpen ? "translate-y-0" : "opacity-0 translate-y-60 pointer-events-none"
           }`}
         >
             <ul className="h-full w-full bg-white pl-6 p-1 py-4 text-sm list-disc text-gray-700 space-y-2">
