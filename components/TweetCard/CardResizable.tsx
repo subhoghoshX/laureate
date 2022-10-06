9ca4fa6a7093c92f57083f02263bb400eba5d78b
Ilya <tryonelove@proton.me>
2022-10-07 01:11:00 +0200

    fix: do not allow resizing card to negative values

--- components/TweetCard/CardResizable.tsx
+++ components/TweetCard/CardResizable.tsx
@@ -1,41 +1,51 @@
 import { useCardStore } from "../../store/card";
-import { Resizable } from "re-resizable";
+import { Resizable, ResizeDirection } from "re-resizable";
 import CardOuter from "./CardOuter";
+
+
+const MIN_ALLOWED_CARD_SIZE = 0;
+
+function calculateNewSize(calculatorCallback: () => number) {
+  const newSize = calculatorCallback();
+
+  return newSize <= MIN_ALLOWED_CARD_SIZE ? MIN_ALLOWED_CARD_SIZE : newSize;
+};
 
 export default function ResizableTweet({ rootRef }: any) {
   const cardWidth = useCardStore((state) => state.width);
   const cardHeight = useCardStore((state) => state.height);
   const setWidth = useCardStore((state) => state.setWidth);
   const setHeight = useCardStore((state) => state.setHeight);
-  function resizeHandler(e: any, dir: any) {
+
+  function resizeHandler(e: any, dir: ResizeDirection) {
     switch (dir) {
       case "left":
-        setWidth((width) => width - e.movementX);
+        setWidth((width) => calculateNewSize(() => width - e.movementX));
         break;
       case "right":
-        setWidth((width) => width + e.movementX);
+        setWidth((width) => calculateNewSize(() => width + e.movementX));
         break;
       case "top":
-        setHeight((height) => height - e.movementY);
+        setHeight((height) => calculateNewSize(() => height - e.movementY));
         break;
       case "bottom":
-        setHeight((height) => height + e.movementY);
+        setHeight((height) => calculateNewSize(() => height + e.movementY));
         break;
       case "topLeft":
-        setWidth((width) => width - e.movementX);
-        setHeight((height) => height - e.movementY);
+        setWidth((width) => calculateNewSize(() => width - e.movementX));
+        setHeight((height) => calculateNewSize(() => height - e.movementY));
         break;
       case "topRight":
-        setWidth((width) => width + e.movementX);
-        setHeight((height) => height - e.movementY);
+        setWidth((width) => calculateNewSize(() => width + e.movementX));
+        setHeight((height) => calculateNewSize(() => height - e.movementY));
         break;
       case "bottomLeft":
-        setWidth((width) => width - e.movementX);
-        setHeight((height) => height + e.movementY);
+        setWidth((width) => calculateNewSize(() => width - e.movementX));
+        setHeight((height) => calculateNewSize(() => height + e.movementY));
         break;
       case "bottomRight":
-        setWidth((width) => width + e.movementX);
-        setHeight((height) => height + e.movementY);
+        setWidth((width) => calculateNewSize(() => width + e.movementX));
+        setHeight((height) => calculateNewSize(() => height + e.movementY));
         break;
     }
   }
