ac267ef5eda4201735278b17cfde6e527d8c24b0
Jaka Daneu <jakad@me.com>
2022-10-10 17:18:54 +0200

    fix: ⚰️ remove `try...catch`

--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -82,24 +82,20 @@ function SaveButton({ rootRef }: any) {
   const { Icon, setShowSecondIcon } = useIcon([SaveSVG, CheckSVG]);
   async function exportPNG() {
     if (rootRef.current !== undefined) {
-      try {
-        const canvas = await html2canvas(rootRef.current, {
-          allowTaint: true,
-          useCORS: true,
-          scale: 3,
-          backgroundColor: null,
-        });
-        const img = canvas.toDataURL("image/png", 1.0);
+      const canvas = await html2canvas(rootRef.current, {
+        allowTaint: true,
+        useCORS: true,
+        scale: 3,
+        backgroundColor: null,
+      });
+      const img = canvas.toDataURL("image/png", 1.0);
 
-        const a = document.createElement("a");
-        a.href = img;
-        a.download = "tweet.png";
-        a.click();
-        a.remove();
-        setShowSecondIcon(true);
-      } catch (error) {
-        // ? do something
-      }
+      const a = document.createElement("a");
+      a.href = img;
+      a.download = "tweet.png";
+      a.click();
+      a.remove();
+      setShowSecondIcon(true);
     }
   }
 
@@ -119,22 +115,18 @@ function CopyButton({ rootRef }: any) {
 
   async function copytoClipboard() {
     if (rootRef.current !== undefined) {
-      try {
-        const canvas = await html2canvas(rootRef.current, {
-          allowTaint: true,
-          useCORS: true,
-          scale: 3,
-          backgroundColor: null,
-        });
+      const canvas = await html2canvas(rootRef.current, {
+        allowTaint: true,
+        useCORS: true,
+        scale: 3,
+        backgroundColor: null,
+      });
 
-        canvas.toBlob((blob: any) => {
-          const data = [new ClipboardItem({ "image/png": blob })];
-          navigator.clipboard.write(data);
-        });
-        setShowSecondIcon(true);
-      } catch (error) {
-        // ? do something
-      }
+      canvas.toBlob((blob: any) => {
+        const data = [new ClipboardItem({ "image/png": blob })];
+        navigator.clipboard.write(data);
+      });
+      setShowSecondIcon(true);
     }
   }
   return (
