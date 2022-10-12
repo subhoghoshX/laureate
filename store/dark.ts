87e275b1f62ca6d396d4d9c3733805f21f011a17
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-10-12 08:47:52 +0530

    feat: theme persitence is now enabled using zustand persist

--- store/dark.ts
+++ store/dark.ts
@@ -1,9 +1,17 @@
 import create from "zustand";
 import { DarkState } from "./types";
+import { persist } from "zustand/middleware";
 
-export const useDarkStore = create<DarkState>((set) => ({
-  isDark: false,
-  setIsDark(callback) {
-    set(({ isDark }) => ({ isDark: callback(isDark) }));
-  },
-}));
+export const useDarkStore = create<DarkState>()(
+  persist(
+    (set) => ({
+      isDark: false,
+      setIsDark(callback) {
+        set(({ isDark }) => ({ isDark: callback(isDark) }));
+      },
+    }),
+    {
+      name: "laureate-dark-mode",
+    },
+  ),
+);
